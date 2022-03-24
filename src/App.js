ddc3e8c3c78d2c0ee66241c77b47e0bd6e50bb1a
shakiru shekoni <shakirushekoni@shakirus-MacBook-Pro.local>
2022-03-24 11:02:11 +0100

    omdbreact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.scss';
+import Logo from './assets/MyTestApp.png'
+import axios from 'axios'
+import _ from 'lodash'
 
 function App() {
+
+  const [data, setdata] =  useState([])
+  const [search, setsearch] = useState('')
+
+  useEffect(async() => {
+      const movies = await axios.get(`http://www.omdbapi.com?i=tt3896198&apikey=7645a5c3&s=${search != '' ? search : 'movies'}`)
+      let groupData = _.groupBy(movies.data.Search, 'Type')
+      setdata(Object.entries(groupData))
+      
+  },[search])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app-bar'>
+          <img src={Logo} alt="logo" />
+        </div>
+      <div className='container'>
+        <div className='title'>
+           Watch something incredible.
+        </div>
+      </div>
+       <div className='body-content'>
+         <label htmlFor='search' >Search</label>
+         <input onChange={e => setsearch(e.target.value) }  id='search' />
+         
+         {
+           data.length && data.map((movie, index) => (
+             <div key={index} className="movie-container">
+               <div className='movie-title'>{ movie[0] }</div>
+                 <div className='layout'>
+                    { movie[1].map((item, index) => (
+                    <div key={index} className='img-container'>
+                    <img src={item.Poster} />
+                    <div className='movie-name'>{ item.Title }</div>
+                  </div>
+                  )) }
+                 </div>
+             </div>
+           ))
+         }
+       </div>
     </div>
   );
 }
